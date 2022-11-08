b28cdf5c58da9b4e1417ff661e427b66d206e9ac
Syed Mansoor ul Hassan Bukhari <114132789+MansoorUlHassan@users.noreply.github.com>
2022-11-07 22:30:19 -0800

    Add files via upload

---
+++ I.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main()
+{
+    for (int rows = 0; rows < 5; rows++)
+    {
+        for (int cols = 0; cols < 5; cols++)
+        {
+            if (rows == 0 || rows == 4 || cols == 2)
+            {
+                cout << "*";
+            }
+            else
+            {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+    getch();
+}
